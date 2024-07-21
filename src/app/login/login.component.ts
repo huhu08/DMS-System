724d3b95cc0fa23e7ec4c4d1dbf6bb82fb3207e2
Huda <hudaelzubair@hotmail.com>
2024-07-21 06:13:45 +0300

    adding login ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from "@angular/core";
+import {Validators, FormGroup, FormBuilder} from "@angular/forms";
+import {Router} from "@angular/router";
+// import { LoginObject } from "../core/models/login-object.model";
+// import {Session} from "../core/models/session.model";
+// import { AuthService } from "../core/services/auth.service";
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  templateUrl: 'login.component.html'
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
+  public submitted: boolean = false;
+  public error: {code: number, message: string} | null = null;
+
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('username');
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+
+  public submitLogin(): void {
+    this.submitted = true;
+    this.error = null;
+    if(this.loginForm.valid){
+      this.authService.login(new LoginObject(this.loginForm.value)).subscribe(
+        data => this.correctLogin(data),
+        error => {
+          this.error = error;
+        }
+      )
+    }
+  }
+
+  private correctLogin(data: Session){
+    this.router.navigate(['/home']);
+  }
 }
