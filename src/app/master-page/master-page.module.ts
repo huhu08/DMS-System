ef236bc213553a75ac8d93cecd8ebef5e84ebc43
Huda <hudaelzubair@hotmail.com>
2024-08-07 13:54:16 +0300

    added master page module

---
+++ src/app/master-page/master-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MasterPageComponent } from './master-page/master-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    MasterPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MasterPageModule { }
